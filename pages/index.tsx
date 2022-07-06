485d7e0ea533008c07fa39a5a32b6ef10564dea9
cfoster5 <cfoster3204@gmail.com>
2022-07-05 21:22:53 -0500

    First major version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,125 @@
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { projects } from "../projects";
 
 const Home: NextPage = () => {
+  // const [angle, setAngle] = useState(8);
+  // const [perspective, setPerspective] = useState(500);
+  // const y = useMotionValue(0.5);
+  // const x = useMotionValue(0.5);
+
+  // const rotateY = useTransform(x, [0, 1], [-angle, angle], {
+  //   clamp: true,
+  // });
+  // const rotateX = useTransform(y, [0, 1], [angle, -angle], {
+  //   clamp: true,
+  // });
+
+  // function onMove(e) {
+  //   // get position information for the card
+  //   const bounds = e.currentTarget.getBoundingClientRect();
+
+  //   // set x,y local coordinates
+  //   const xValue = (e.clientX - bounds.x) / e.currentTarget.clientWidth;
+  //   const yValue = (e.clientY - bounds.y) / e.currentTarget.clientHeight;
+
+  //   // update MotionValues
+  //   x.set(xValue, true);
+  //   y.set(yValue, true);
+  // }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-zinc-900 dark:text-white">
+      {/* <div className="flex min-h-screen flex-col items-center justify-center py-2"> */}
       <Head>
-        <title>Create Next App</title>
+        <title>Corey Foster | Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="title" content="Corey Foster | Portfolio" />
+        <meta
+          name="description"
+          content="A web and app developer based in Evansville, IN"
+        />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="p-8 sm:p-20">
+        <div className="text-center">
+          <h1 className="text-6xl font-bold">
+            Hi, I'm{" "}
+            <span className="text-blue-600 dark:text-blue-500">Corey</span>.
+          </h1>
+          <p className="mt-3 text-2xl">
+            A web and app developer based in Evansville, IN
+          </p>
         </div>
+
+        <div className="max-w-4xl">
+          <h2 className="mt-6 text-4xl font-bold">About Me</h2>
+          <p className="mt-3">
+            I'm currently an Application Analyst in the financial services
+            industry, specializing in SharePoint and API development. I lead
+            custom development in SharePoint using React and Angular.
+          </p>
+          <p>
+            I love movies, shows, and games and often combine these interests
+            with apps or sites I'm working on in my free time.
+          </p>
+          <h2 className="mt-6 text-4xl font-bold">Projects</h2>
+        </div>
+
+        <div className="mt-3 block gap-4 space-y-4 sm:flex sm:space-y-0">
+          {projects.map((project, index) => (
+            <div key={index} className="flex w-full flex-col flex-nowrap gap-4">
+              <motion.div
+                // className="relative h-96 rounded-lg bg-blue-500 sm:w-80"
+                className="relative h-[30rem] cursor-pointer overflow-hidden sm:w-auto"
+                // className="relative aspect-[195/422] rounded-lg bg-blue-500"
+                whileHover={{ scale: 1.025 }}
+              >
+                <div
+                  className="absolute block h-full w-full rounded-lg"
+                  style={{
+                    background: `linear-gradient(0deg, rgb(34, 35, 38) 9%, rgba(33, 34, 37, 0.89) 42%, rgba(34, 35, 38, 0) 156%), url(${project.img}) center top / cover`,
+                  }}
+                ></div>
+                <div className="absolute bottom-0 p-4 pt-0">
+                  <p className="text-xl font-bold text-white">{project.name}</p>
+                  <div className="flex flex-wrap gap-3">
+                    {project.tags.map((tag) => (
+                      <span
+                        key={tag}
+                        className="text-sm text-zinc-300 dark:text-zinc-400"
+                      >
+                        {tag}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </motion.div>
+            </div>
+          ))}
+        </div>
+
+        {/* <div className="max-w-4xl">
+          <h2 className="mt-6 text-4xl font-bold">Contact</h2>
+          <div className="mt-3 flex">
+            <Image
+              src="/paper-plane.svg"
+              alt="paper-plane"
+              width={16}
+              height={16}
+            />
+            <span className="ml-2">Email</span>
+          </div>
+        </div> */}
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className="flex h-8 w-full items-center justify-center border-t p-8">
+        <p className="flex items-center justify-center gap-2">
+          Powered by Next.js and Tailwind CSS
+        </p>
       </footer>
     </div>
   );
