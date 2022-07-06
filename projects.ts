5cbeed585911b2c1319fdad26777c3ee88936e60
cfoster5 <cfoster3204@gmail.com>
2022-07-05 21:20:26 -0500

    Added projects and related assets

---
+++ projects.ts
@@ -0,0 +1,31 @@
+export const projects = [
+  {
+    name: "LookForward",
+    description:
+      "App developed with React Native, Firebase, The Movie Database (TMDB) API, and Twitch's IGDB API. Keep track of upcoming movie and game releases.",
+    img: "/lookforward.png",
+    tags: [
+      "react native",
+      "firebase",
+      "push notifications",
+      "tmdb api",
+      "igdb api",
+    ],
+    link: "https://getlookforward.app",
+  },
+  {
+    name: "Marvel Ratings Web App",
+    description:
+      "Web app developed with Angular, Ionic, Firebase and The Movie Database API that stores your rankings for the Marvel Cinematic Universe.",
+    img: "/mcu_ratings.png",
+    tags: ["angular", "ionic", "firebase", "tmdb api"],
+    link: "https://marvelratings.web.app",
+  },
+  {
+    name: "Nudget",
+    description:
+      "Hybrid app developed with Angular, Ionic, Firebase, and Twilio that aids in family management and focuses on a shared and intuitive grocery list.",
+    img: "/nudget.png",
+    tags: ["angular", "ionic", "firebase", "twilio api"],
+  },
+];
