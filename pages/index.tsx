25641e990e8142175f4880bc1ce56e56f1cc428b
cfoster5 <cfoster3204@gmail.com>
2022-11-10 09:26:28 -0600

    Extracted ProjectCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,45 @@ const Popover = () => {
   );
 };
 
+const ProjectCard = ({ project }: { project: typeof projects[0] }) => (
+  <div className="flex w-full flex-col flex-nowrap gap-4">
+    <motion.div
+      // className="relative h-96 rounded-lg bg-blue-500 sm:w-80"
+      className="relative h-[30rem] cursor-pointer overflow-hidden rounded-lg sm:w-auto"
+      // className="relative aspect-[195/422] rounded-lg bg-blue-500"
+      whileHover={{ scale: 1.025 }}
+    >
+      {/* TODO: fix image peeking through at borders */}
+      <Image
+        src={project.img}
+        alt=""
+        fill
+        className="rounded-lg object-cover object-top"
+      />
+      <div
+        className="absolute block h-full w-full rounded-lg"
+        style={{
+          background: `linear-gradient(0deg, rgb(34, 35, 38) 9%, rgba(33, 34, 37, 0.89) 42%, rgba(34, 35, 38, 0) 156%)`,
+        }}
+      />
+      {/*  */}
+      <div className="absolute bottom-0 p-4 pt-0">
+        <p className="text-xl font-bold text-white">{project.name}</p>
+        <div className="flex flex-wrap gap-3">
+          {project.tags.map((tag) => (
+            <span
+              key={tag}
+              className="text-sm text-zinc-300 dark:text-zinc-400"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </motion.div>
+  </div>
+);
+
 const Home: NextPage = () => {
   // const [angle, setAngle] = useState(8);
   // const [perspective, setPerspective] = useState(500);
@@ -103,47 +142,7 @@ const Home: NextPage = () => {
 
           <div className="mt-3 block gap-4 space-y-4 sm:flex sm:space-y-0">
             {projects.map((project, index) => (
-              <div
-                key={index}
-                className="flex w-full flex-col flex-nowrap gap-4"
-              >
-                <motion.div
-                  // className="relative h-96 rounded-lg bg-blue-500 sm:w-80"
-                  className="relative h-[30rem] cursor-pointer overflow-hidden sm:w-auto"
-                  // className="relative aspect-[195/422] rounded-lg bg-blue-500"
-                  whileHover={{ scale: 1.025 }}
-                >
-                  <Image
-                    src={project.img}
-                    alt=""
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="top"
-                    className="rounded-lg"
-                  />
-                  <div
-                    className="absolute block h-full w-full rounded-lg"
-                    style={{
-                      background: `linear-gradient(0deg, rgb(34, 35, 38) 9%, rgba(33, 34, 37, 0.89) 42%, rgba(34, 35, 38, 0) 156%)`,
-                    }}
-                  />
-                  <div className="absolute bottom-0 p-4 pt-0">
-                    <p className="text-xl font-bold text-white">
-                      {project.name}
-                    </p>
-                    <div className="flex flex-wrap gap-3">
-                      {project.tags.map((tag) => (
-                        <span
-                          key={tag}
-                          className="text-sm text-zinc-300 dark:text-zinc-400"
-                        >
-                          {tag}
-                        </span>
-                      ))}
-                    </div>
-                  </div>
-                </motion.div>
-              </div>
+              <ProjectCard key={index} project={project} />
             ))}
           </div>
 
