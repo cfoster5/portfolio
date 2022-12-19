5b8ad2e60c7b8076ebeb4b6bfddd83edd8364c75
cfoster5 <cfoster3204@gmail.com>
2022-12-18 22:55:17 -0600

    Removed unnecessary React fragment

--- app/page.tsx
+++ app/page.tsx
@@ -6,75 +6,73 @@ import { LayoutWrapper } from "./LayoutWrapper";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-zinc-900 dark:text-white">
-        <main className="p-8 sm:p-20">
-          <div className="text-center">
-            <h1 className="text-6xl font-bold">
-              Hi, I&apos;m{" "}
-              <span className="text-blue-600 dark:text-blue-500">Corey</span>.
-            </h1>
-            <p className="mt-3 text-2xl">
-              A web and app developer based in Evansville, IN
-            </p>
-          </div>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-zinc-900 dark:text-white">
+      <main className="p-8 sm:p-20">
+        <div className="text-center">
+          <h1 className="text-6xl font-bold">
+            Hi, I&apos;m{" "}
+            <span className="text-blue-600 dark:text-blue-500">Corey</span>.
+          </h1>
+          <p className="mt-3 text-2xl">
+            A web and app developer based in Evansville, IN
+          </p>
+        </div>
 
-          <div className="max-w-4xl">
-            <h2 className="mt-6 text-4xl font-bold">About Me</h2>
-            <p className="mt-3">
-              I&apos;m currently an Application Analyst in the financial
-              services industry, specializing in SharePoint and API development.
-              I lead custom development in SharePoint using React and Angular.
-            </p>
-            <p>
-              I love movies, shows, and games and often combine these interests
-              with apps or sites I&apos;m working on in my free time.
-            </p>
-            <h2 className="mt-6 text-4xl font-bold">Projects</h2>
-          </div>
+        <div className="max-w-4xl">
+          <h2 className="mt-6 text-4xl font-bold">About Me</h2>
+          <p className="mt-3">
+            I&apos;m currently an Application Analyst in the financial services
+            industry, specializing in SharePoint and API development. I lead
+            custom development in SharePoint using React and Angular.
+          </p>
+          <p>
+            I love movies, shows, and games and often combine these interests
+            with apps or sites I&apos;m working on in my free time.
+          </p>
+          <h2 className="mt-6 text-4xl font-bold">Projects</h2>
+        </div>
 
-          <div className="mt-3 flex flex-col gap-4 sm:flex-row">
-            <LayoutWrapper />
-          </div>
+        <div className="mt-3 flex flex-col gap-4 sm:flex-row">
+          <LayoutWrapper />
+        </div>
 
-          <div className="max-w-4xl">
-            <h2 className="mt-6 text-4xl font-bold">Contact</h2>
-            <div className="mt-3 flex">
-              <Link
-                href="mailto:cfoster3204@gmail.com"
-                className="flex items-center hover:underline"
-              >
-                <FaPaperPlane />
-                <span className="ml-2">cfoster3204@gmail.com</span>
-              </Link>
-            </div>
-            <div className="mt-3 flex">
-              <Link
-                href="https://github.com/cfoster5"
-                className="flex items-center hover:underline"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaGithub />
-                <span className="ml-2">GitHub</span>
-              </Link>
-            </div>
+        <div className="max-w-4xl">
+          <h2 className="mt-6 text-4xl font-bold">Contact</h2>
+          <div className="mt-3 flex">
+            <Link
+              href="mailto:cfoster3204@gmail.com"
+              className="flex items-center hover:underline"
+            >
+              <FaPaperPlane />
+              <span className="ml-2">cfoster3204@gmail.com</span>
+            </Link>
           </div>
-        </main>
+          <div className="mt-3 flex">
+            <Link
+              href="https://github.com/cfoster5"
+              className="flex items-center hover:underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub />
+              <span className="ml-2">GitHub</span>
+            </Link>
+          </div>
+        </div>
+      </main>
 
-        <footer className="flex h-8 w-full items-center justify-center border-t p-8">
-          <Link
-            className="flex items-center justify-center gap-2 hover:underline"
-            href="https://github.com/cfoster5/portfolio"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <RiGitRepositoryLine />
-            Powered by Next.js and Tailwind CSS
-          </Link>
-        </footer>
-      </div>
-    </>
+      <footer className="flex h-8 w-full items-center justify-center border-t p-8">
+        <Link
+          className="flex items-center justify-center gap-2 hover:underline"
+          href="https://github.com/cfoster5/portfolio"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <RiGitRepositoryLine />
+          Powered by Next.js and Tailwind CSS
+        </Link>
+      </footer>
+    </div>
   );
 };
 
