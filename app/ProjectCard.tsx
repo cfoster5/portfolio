4c4cff444ba897f88944e524b2060432e1557a07
cfoster5 <cfoster3204@gmail.com>
2025-03-31 21:02:04 -0500

    refactor: Remove motion hover animation in favor of CSS

--- app/ProjectCard.tsx
+++ app/ProjectCard.tsx
@@ -1,43 +1,34 @@
-"use client";
-import { motion } from "framer-motion";
 import Image from "next/image";
 import { projects } from "../projects";
 
-export const ProjectCard = ({ project }: { project: (typeof projects)[0] }) => (
-  <div className="w-full flex-1 flex-grow-0">
-    <motion.div
-      // className="relative h-96 rounded-lg bg-blue-500 sm:w-80"
-      className="relative h-[30rem] cursor-pointer overflow-hidden rounded-lg sm:aspect-[9/16] sm:w-auto"
-      // className="relative aspect-[195/422] rounded-lg bg-blue-500"
-      whileHover={{ scale: 1.025 }}
-    >
-      {/* TODO: fix image peeking through at borders */}
-      <Image
-        src={project.img}
-        alt=""
-        fill
-        className="rounded-lg object-cover object-top"
-      />
-      <div
-        className="absolute block h-full w-full rounded-lg"
-        style={{
-          background: `linear-gradient(0deg, rgb(34, 35, 38) 9%, rgba(33, 34, 37, 0.89) 42%, rgba(34, 35, 38, 0) 156%)`,
-        }}
-      />
-      {/*  */}
-      <div className="absolute bottom-0 p-4 pt-0">
-        <h3 className="text-xl font-bold text-white">{project.name}</h3>
-        <div className="flex flex-wrap gap-3">
-          {project.tags.map((tag) => (
-            <span
-              key={tag}
-              className="text-sm text-zinc-300 dark:text-zinc-400"
-            >
-              {tag}
-            </span>
-          ))}
-        </div>
+export const ProjectCard = ({
+  project,
+}: {
+  project: (typeof projects)[number];
+}) => (
+  <div className="relative h-[30rem] cursor-pointer overflow-hidden rounded-lg transition-transform hover:scale-105 sm:aspect-[9/16] sm:w-auto">
+    {/* TODO: fix image peeking through at borders */}
+    <Image
+      src={project.img}
+      alt=""
+      fill
+      className="rounded-lg object-cover object-top"
+    />
+    <div
+      className="absolute block h-full w-full rounded-lg"
+      style={{
+        background: `linear-gradient(0deg, rgb(34, 35, 38) 9%, rgba(33, 34, 37, 0.89) 42%, rgba(34, 35, 38, 0) 156%)`,
+      }}
+    />
+    <div className="absolute bottom-0 p-4 pt-0">
+      <h3 className="text-xl font-bold text-white">{project.name}</h3>
+      <div className="flex flex-wrap gap-3">
+        {project.tags.map((tag) => (
+          <span key={tag} className="text-sm text-zinc-300 dark:text-zinc-400">
+            {tag}
+          </span>
+        ))}
       </div>
-    </motion.div>
+    </div>
   </div>
 );
