ecd33c6a002b7da21b117354cdeb015f18fe2ecf
cfoster5 <cfoster3204@gmail.com>
2025-12-28 19:00:08 -0600

    feat: add project details for LookForward and Simply Water, including icons and descriptions

--- projects.ts
+++ projects.ts
@@ -1,8 +1,10 @@
 export const projects = [
   {
     name: "LookForward",
+    slug: "lookforward",
     description:
-      "App developed with React Native, Firebase, The Movie Database (TMDB) API, and Twitch's IGDB API. Keep track of upcoming movie and game releases.",
+      "Track upcoming movies and video games, so you always know what's next.",
+    icon: "/lookforward-icon.png",
     img: "/lookforward.png",
     tags: [
       "react native",
@@ -12,15 +14,38 @@ export const projects = [
       "igdb api",
     ],
     link: "https://getlookforward.app",
+    color: "bg-gradient-to-b from-indigo-700 to-purple-950",
   },
   {
-    name: "Marvel Ratings Web App",
-    description:
-      "Web app developed with Angular, Ionic, Firebase and The Movie Database API that stores your rankings for the Marvel Cinematic Universe.",
-    img: "/mcu_ratings.png",
-    tags: ["angular", "ionic", "firebase", "tmdb api"],
-    link: "https://marvelratings.web.app",
+    name: "Simply Water",
+    slug: "simply-water",
+    description: "Stay on top of your daily water intake with ease.",
+    icon: "/simply-water.png",
+    img: "/lookforward.png",
+    tags: ["react native", "expo"],
+    link: "/simply-water",
+    color: "bg-blue-500",
   },
+  // {
+  //   name: "PTO Pacer",
+  //   slug: "pto-pacer",
+  //   description:
+  //     "Prevent burnout and track your time-off usage throughout the year. Upload your PTO data and visualize your vacation patterns.",
+  //   icon: "/PTOPacer_icon.png",
+  //   img: "/lookforward.png",
+  //   tags: ["react", "next.js"],
+  //   link: "/pto-pacer",
+  //   color: "bg-gradient-to-b from-blue-500 to-purple-600",
+  // },
+  // {
+  //   name: "Marvel Ratings Web App",
+  //   description:
+  //     "Web app developed with Angular, Ionic, Firebase and The Movie Database API that stores your rankings for the Marvel Cinematic Universe.",
+  //   img: "/mcu_ratings.png",
+  //   tags: ["angular", "ionic", "firebase", "tmdb api"],
+  //   link: "https://marvelratings.web.app",
+  //   color: "bg-gradient-to-b from-blue-500 to-purple-500",
+  // },
   {
     name: "Devanco Foods",
     description:
